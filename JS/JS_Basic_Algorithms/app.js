be0cb03eacf3f5e6b8dc174317821f3d203d6830
kevinD2413 <kevindanielgeraldo123@gmail.com>
2024-01-22 09:55:45 +0100

    Iteration#5

--- JS/JS_Basic_Algorithms/app.js
+++ JS/JS_Basic_Algorithms/app.js
@@ -31,3 +31,115 @@ let y = 10
 //!--------------------------------------------------------------------------
 
 let z = h + y 
+
+
+/*
+1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = {name: 'Jack Sparrow', age: 10};
+
+1.2 Declara 3 variables con los nombres y valores siguientes 
+	firstName = 'Jon'; 
+	lastName = 'Snow'; 
+	age = 24; 
+Muestralos por consola de esta forma: 
+	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+
+1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+ambos juguetes.
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+/
+1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+basePrice más el valor de la variable globalBasePrice.
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+*/
+
+//!--------------------------------------------------------------------------
+//?1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = {name: 'Jack Sparrow', age: 10};
+//!--------------------------------------------------------------------------
+
+
+//!--------------------------------------------------------------------------
+//?1.2 Declara 3 variables con los nombres y valores siguientes 
+	firstName = 'Jon'; 
+	lastName = 'Snow';
+	age = 24; 
+//?Muestralos por consola de esta forma: 
+	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+//!--------------------------------------------------------------------------
+
+const firstName = 'Jon';
+const lastName = 'Snow';
+const age = 24;
+const nombreCompleto = ('Soy ${firstName} ${lastName}, tengo ${age} y me gustan los lobos.');
+console.log("🚀 ~ nombreCompleto:", nombreCompleto)
+
+
+//!--------------------------------------------------------------------------
+
+//?1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+//?ambos juguetes.
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+//!--------------------------------------------------------------------------
+
+
+
+//!--------------------------------------------------------------------------
+//?1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+//?y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+//?basePrice más el valor de la variable globalBasePrice.
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+//!--------------------------------------------------------------------------
+
+
+
+/*
+Iteración #3: Operadores
+1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.
+
+1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.
+*/
+
+//!--------------------------------------------------------------------------
+//? 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+//!--------------------------------------------------------------------------
+
+let T = 10;
+T *= 5;
+console.log(T);
+
+
+//!--------------------------------------------------------------------------
+//?1.2 Divide 10 por 2 y muestra el resultado en un console.
+//!--------------------------------------------------------------------------
+
+let K = 10;
+K /= 2;
+console.log(K);
+
+//!--------------------------------------------------------------------------
+//?1.3 Muestra mediante un console el resto de dividir 15 por 9.
+//!--------------------------------------------------------------------------
+
+
+
+//!--------------------------------------------------------------------------
+//?1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+//? teniendo dos variables p = 10 y j = 5.
+//!--------------------------------------------------------------------------
+
